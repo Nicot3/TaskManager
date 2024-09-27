e302d40c2a681cddb5dd1870283f868bc0a90ac9
Nico Tena <nicoteja@hotmail.com>
2024-09-27 19:23:49 +0200

    feat: Add domain Unit Tests

---
+++ src/TaskManager.Domain.Tests/TodoTaskTest.cs
@@ -0,0 +1,75 @@
+using AutoFixture;
+using FluentAssertions;
+using System.Xml.Linq;
+using TaskManager.Domain.TodoTasks;
+
+namespace TaskManager.Domain.Tests
+{
+    public class TodoTaskTest
+    {
+        [Fact]
+        public void TodoTask_AddTag_ShouldAddTag()
+        {
+            //Arrange
+            var fixture = new Fixture();
+
+            var tag = fixture.Create<string>();
+            var todoTask = fixture.Create<TodoTask>();
+            //Act
+            todoTask.AddTag(tag);
+            //Assert
+            todoTask.Tags.Should().NotBeEmpty();
+            todoTask.Tags.Should().OnlyContain(i => i.Name == tag);
+        }
+
+        [Fact]
+        public void TodoTask_RemoveTag_ShouldRemoveTag()
+        {
+            //Arrange
+            var fixture = new Fixture();
+
+            var tag = fixture.Create<string>();
+            var todoTask = fixture.Create<TodoTask>();
+            //Act
+            todoTask.AddTag(tag);
+            todoTask.RemoveTag(tag);
+            //Assert
+            todoTask.Tags.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void TodoTask_AddTagWithExistingTag_ShouldThrowArgumentException()
+        {
+            //Arrange
+            var fixture = new Fixture();
+
+            var tag = fixture.Create<string>();
+            var todoTask = fixture.Create<TodoTask>();
+            //Act
+            todoTask.AddTag(tag);
+            Action act = () => todoTask.AddTag(tag);
+            //Assert
+            todoTask.Tags.Should().NotBeEmpty();
+            todoTask.Tags.Should().OnlyContain(i => i.Name == tag);
+            act.Should().Throw<ArgumentException>().WithMessage($"Tag with name {tag} already exists");
+        }
+
+        [Fact]
+        public void TodoTask_RemoveTagWithoutExistingTag_ShouldThrowArgumentException()
+        {
+            //Arrange
+            var fixture = new Fixture();
+
+            var tag = fixture.Create<string>();
+            var todoTask = fixture.Create<TodoTask>();
+            //Act
+            todoTask.AddTag(fixture.Create<string>());
+            Action act = () => todoTask.RemoveTag(tag);
+            //Assert
+            todoTask.Tags.Should().NotBeEmpty();
+            todoTask.Tags.Should().NotContain(i => i.Name == tag);
+
+            act.Should().Throw<ArgumentException>().WithMessage($"Tag with name {tag} doesn't exists in the {todoTask.Id} task tags");
+        }
+    }
+}
