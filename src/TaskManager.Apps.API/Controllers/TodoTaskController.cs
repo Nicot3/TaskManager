84f37b4bb9fcb313991259855be33d6bc11ec6a1
Nico Tena <nicoteja@hotmail.com>
2024-10-06 19:32:53 +0200

    feat: Add GetTodoTasksQuery and AddTagCommand to API TodoTaskController

--- src/TaskManager.Apps.API/Controllers/TodoTaskController.cs
+++ src/TaskManager.Apps.API/Controllers/TodoTaskController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TaskManager.Application.Common.Contracts.Commands;
 using TaskManager.Application.Common.Contracts.Queries;
+using TaskManager.Application.Common.Exceptions;
+using TaskManager.Application.TodoTasks.Commands.AddTag;
 using TaskManager.Application.TodoTasks.Commands.CreateTodoTask;
 using TaskManager.Application.TodoTasks.Queries.GetTodoTaskById;
-using TaskManager.Domain.TodoTasks;
+using TaskManager.Application.TodoTasks.Queries.GetTodoTasks;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,26 +16,41 @@ namespace TaskManager.Apps.API.Controllers
     public class TodoTaskController : ControllerBase
     {
         private readonly ICommandHandler<CreateTodoTaskCommand, string> _createTodoTaskCommandHandler;
+        private readonly ICommandHandler<AddTagCommand, bool> _addTagCommandHandler;
+
         private readonly IQueryHandler<GetTodoTaskByIdQuery, GetTodoTaskByIdQueryResult> _getTodoTaskByIdQueryResult;
+        private readonly IQueryHandler<GetTodoTasksQuery, GetTodoTasksQueryResult> _getTodoTasksQueryResult;
 
         public TodoTaskController(ICommandHandler<CreateTodoTaskCommand, string> createTodoTaskCommandHandler,
-                                  IQueryHandler<GetTodoTaskByIdQuery, GetTodoTaskByIdQueryResult> getTodoTaskByIdQueryResult)
+                                  IQueryHandler<GetTodoTaskByIdQuery, GetTodoTaskByIdQueryResult> getTodoTaskByIdQueryResult,
+                                  IQueryHandler<GetTodoTasksQuery, GetTodoTasksQueryResult> getTodoTasksQueryResult,
+                                  ICommandHandler<AddTagCommand, bool> addTagCommandHandler)
         {
             _createTodoTaskCommandHandler = createTodoTaskCommandHandler;
             _getTodoTaskByIdQueryResult = getTodoTaskByIdQueryResult;
+            _getTodoTasksQueryResult = getTodoTasksQueryResult;
+            _addTagCommandHandler = addTagCommandHandler;
         }
 
 
         // GET: api/<TodoTaskController>
         [HttpGet]
-        public async Task<IEnumerable<TodoTask>> GetAsync()
+        public async Task<IActionResult> GetAsync()
         {
-            throw new NotImplementedException();
+            try
+            {
+                var todoTasks = await _getTodoTasksQueryResult.ExecuteAsync(new());
+                return Ok(todoTasks);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
         }
 
         // GET api/<TodoTaskController>/5
         [HttpGet("{id}")]
-        public async Task<IActionResult> Get([FromRoute] string id)
+        public async Task<IActionResult> GetAsync([FromRoute] string id)
         {
             try
             {
@@ -44,15 +61,20 @@ namespace TaskManager.Apps.API.Controllers
 
                 return Ok(todoTask);
             }
-            catch (Exception e)
+            catch (EntityNotFoundException e)
             {
                 return BadRequest(e.Message);
             }
         }
 
         // POST api/<TodoTaskController>
+        /// <summary>
+        /// Create a new task
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
         [HttpPost]
-        public async Task<IActionResult> Post([FromBody] CreateTodoTaskCommand command)
+        public async Task<IActionResult> PostAsync([FromBody] CreateTodoTaskCommand command)
         {
             try
             {
@@ -66,8 +88,13 @@ namespace TaskManager.Apps.API.Controllers
         }
 
         // PUT api/<TodoTaskController>/5
+        /// <summary>
+        /// Update existing task
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="command"></param>
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put(int id, [FromBody] string command)
         {
             throw new NotImplementedException();
         }
@@ -77,6 +104,25 @@ namespace TaskManager.Apps.API.Controllers
         public void Delete(int id)
         {
             throw new NotImplementedException();
+        }
+
+        [HttpPost("{id}/Tag")]
+        public async Task<IActionResult> AddTagAsync([FromRoute] string id, [FromBody] string name)
+        {
+            try
+            {
+                var result = await _addTagCommandHandler.HandleAsync(new()
+                {
+                    TodoTaskId = id,
+                    Name = name
+                });
+
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
         }
     }
 }
